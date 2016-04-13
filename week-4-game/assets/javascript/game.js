03553407692040aab85a511dec6a18f6f71fd409
RacquelO <RacquelO@users.noreply.github.com>
2016-04-13 15:09:57 -0400

    week4done

--- week-4-game/assets/javascript/game.js
+++ week-4-game/assets/javascript/game.js
@@ -1,191 +1,202 @@
-// global variables
-var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var wordOptions = ["Achelous", "Adonis", "Adrestia", "Aeolus", "Aether", "Agathodaemon", "Aglaea", "Algea", "Alpheus", "Amphitrite", "Ananke", "Androktasiai", "Anemoi", "Anteros", "Apate",  "Aristaeus", "Asclepius", "Asopus",  "Astraea", "Ate", "Atropos", "Aura", "Bia", "Britomartis", "Caerus", "Calypso", "Castalia", "Cephissus", "Chaos", "Chronos", "Circe", "Clotho", "Clytie", "Daphne", "Deimos", "Demeter", "Dionysus", "Dysnomia", "Echo", "Eileithyia", "Eirene", "Elpis", "Endeis", "Enyo", "Erebus", "Eris", "Eros", "Euphrosyne", "Gaea", "Geras", "Graeae", "Harmonia", "Hebe", "Hecate", "Heliadae", "Heliades", "Helios", "Hemera", "Hermaphroditus", "Hesperides", "Hesperus", "Horae", "Horkos", "Hyades", "Hyas", "Hygeia", "Hypnos", "Iasion", "Iris", "Keres", "Keto", "Kratos", "Lachesis", "Lethe", "Limos", "Lyssa", "Makhai", "Maniae", "Melinoe", "Minor", "Gods", "Momus", "Moros", "Morpheus", "Nemesis", "Nephele", "Nike", "Nilus", "Nysiads", "Nyx", "Oenone", "Oizys", "Oneiroi", "Ourea", "Paean",  "Pan", "Persephone", "Pheme", "Philotes","Phobos", "Phonoi", "Phorcys", "Phosphorus", "Phthonus", "Ploutos", "Ponos", "Pontus", "Primordial", "Pseudologoi", "Psyche", "Rhode", "Scamander", "Syrinx", "Tartarus", "Telesto", "Thalassa", "Thalia", "Thanatos", "Thaumas", "Thetis", "Tyche", "Typhoeus", "Uranus", "Zagreus", "Zelus" ];
-var selectedWord = "";
-var lettersinWord = [];
-var numBlanks = 0;
-var blanksAndSuccesses = [];
-var wrongLetters = [];
+$(document).ready(function() {
 
-// test the variables
-console.log(alphabet + ' ' + wordOptions);
+  // ----------------------------------------------------------------
+  // Create 4 characters with objects.
+  var characters = {
+    'Xena': {
+      name: 'Xena',
+      health: 120,
+      attack: 8,
+      imageUrl: "week-4-game/assets/images/xena.gif",
+      enemyAttackBack: 15
+    },
+    'Gabrielle': {
+      name: 'Gabrielle',
+      health: 100,
+      attack: 14,
+      imageUrl: "week-4-game/assets/images/gabrielle.gif",
+      enemyAttackBack: 5
+    },
+    'Joxer': {
+      name: 'Joxer',
+      health: 150,
+      attack: 8,
+      imageUrl: "week-4-game/assets/images/joxer.gif",
+      enemyAttackBack: 20
+    },
+    'Aries': {
+      name: 'Aries',
+      health: 180,
+      attack: 7,
+      imageUrl: "week-4-game/assets/images/aries.gif",
+      enemyAttackBack: 25
+    }
+  };
+  var currSelectedCharacter;
+  var currDefender;
+  var combatants = [];
+  var indexOfSelChar;
+  var attackResult;
+  var turnCounter = 1;
+  var killCount = 0;
 
-//game counter
-var winCount = 0;
-var lossCount = 0;
-var guessesLeft = 9;
+  // ----------------------------------------------------------------
+  // Create function to render to DOM
+  var renderOne = function(character, renderArea, makeChar) {
+    //character: obj, renderArea: class/id, makeChar: string
+    var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+    var charName = $("<div class='character-name'>").text(character.name);
+    var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
+    var charHealth = $("<div class='character-health'>").text(character.health);
+    charDiv.append(charName).append(charImage).append(charHealth);
+    $(renderArea).append(charDiv);
+    // conditional render
+    if (makeChar == 'enemy') {
+      $(charDiv).addClass("enemy");
+    } else if (makeChar == 'defender') {
+      currDefender = character;
+      $(charDiv).addClass("target-enemy");
+    }
+  };
 
-//functions
-function startGame () {
-	selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+  // Create function to render game message to DOM
+  var renderMessage = function(message) {
+    var gameMesageSet = $("#gameMessage");
+    var newMessage = $("<div>").text(message);
+    gameMesageSet.append(newMessage);
 
-}
+    if (message == 'clearMessage') {
+      gameMesageSet.text('');
+    }
+  };
 
-//main process
+  var renderCharacters = function(charObj, areaRender) {
+    //render all characters
+    if (areaRender == '#characters-section') {
+      $(areaRender).empty();
+      for (var key in charObj) {
+        if (charObj.hasOwnProperty(key)) {
+          renderOne(charObj[key], areaRender, '');
+        }
+      }
+    }
+    //render player character
+    if (areaRender == '#selected-character') {
+      renderOne(charObj, areaRender, '');
+    }
+    //render combatants
+    if (areaRender == '#available-to-attack-section') {
+      for (var i = 0; i < charObj.length; i++) {
+        renderOne(charObj[i], areaRender, 'enemy');
+      }
+      //render one enemy to defender area
+      $(document).on('click', '.enemy', function() {
+        //select an combatant to fight
+        name = ($(this).data('name'));
+        //if defernder area is empty
+        if ($('#defender').children().length === 0) {
+          renderCharacters(name, '#defender');
+          $(this).hide();
+          renderMessage("clearMessage");
+        }
+      });
+    }
+    //render defender
+    if (areaRender == '#defender') {
+      $(areaRender).empty();
+      for (var i = 0; i < combatants.length; i++) {
+        //add enemy to defender area
+        if (combatants[i].name == charObj) {
+          renderOne(combatants[i], areaRender, 'defender');
+        }
+      }
+    }
+    //re-render defender when attacked
+    if (areaRender == 'playerDamage') {
+      $('#defender').empty();
+      renderOne(charObj, '#defender', 'defender');
+    }
+    //re-render player character when attacked
+    if (areaRender == 'enemyDamage') {
+      $('#selected-character').empty();
+      renderOne(charObj, '#selected-character', '');
+    }
+    //render defeated enemy
+    if (areaRender == 'enemyDefeated') {
+      $('#defender').empty();
+      var gameStateMessage = "You have defated " + charObj.name + ", you can choose to fight another enemy.";
+      renderMessage(gameStateMessage);
+    }
+  };
+  //this is to render all characters for user to choose their computer
+  renderCharacters(characters, '#characters-section');
+  $(document).on('click', '.character', function() {
+    name = $(this).data('name');
+    //if no player char has been selected
+    if (!currSelectedCharacter) {
+      currSelectedCharacter = characters[name];
+      for (var key in characters) {
+        if (key != name) {
+          combatants.push(characters[key]);
+        }
+      }
+      $("#characters-section").hide();
+      renderCharacters(currSelectedCharacter, '#selected-character');
+      //this is to render all characters for user to choose fight against
+      renderCharacters(combatants, '#available-to-attack-section');
+    }
+  });
 
+  // ----------------------------------------------------------------
+  // Create functions to enable actions between objects.
+  $("#attack-button").on("click", function() {
+    //if defernder area has enemy
+    if ($('#defender').children().length !== 0) {
+      //defender state change
+      var attackMessage = "You attacked " + currDefender.name + " for " + (currSelectedCharacter.attack * turnCounter) + " damage.";
+      renderMessage("clearMessage");
+      //combat
+      currDefender.health = currDefender.health - (currSelectedCharacter.attack * turnCounter);
 
+      //win condition
+      if (currDefender.health > 0) {
+        //enemy not dead keep playing
+        renderCharacters(currDefender, 'playerDamage');
+        //player state change
+        var counterAttackMessage = currDefender.name + " attacked you back for " + currDefender.enemyAttackBack + " damage.";
+        renderMessage(attackMessage);
+        renderMessage(counterAttackMessage);
 
+        currSelectedCharacter.health = currSelectedCharacter.health - currDefender.enemyAttackBack;
+        renderCharacters(currSelectedCharacter, 'enemyDamage');
+        if (currSelectedCharacter.health <= 0) {
+          renderMessage("clearMessage");
+          restartGame("You been defeated...GAME OVER!!!");
+          $("#attack-button").unbind("click");
+        }
+      } else {
+        renderCharacters(currDefender, 'enemyDefeated');
+        killCount++;
+        if (killCount >= 3) {
+          renderMessage("clearMessage");
+          restartGame("You Won!!!! GAME OVER!!!");
+        }
+      }
+      turnCounter++;
+    } else {
+      renderMessage("clearMessage");
+      renderMessage("No enemy here.");
+    }
+  });
 
+  var restartGame = function(inputEndGame) {
+    //When 'Restart' button is clicked, reload the page.
+    var restart = $('<button>Restart</button>').click(function() {
+      location.reload();
+    });
+    var gameState = $("<div>").text(inputEndGame);
+    $("body").append(gameState);
+    $("body").append(restart);
+  };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // checking letter input
-// function letInput(letter){
-// 	if (alphabet.indexOf(letter) != -1){
-// 		return true;
-// 	}
-// }
-
-// //
-// var game = {
-
-// 	// counters and containers
-// 	guesses : 0,
-// 	wins : 0,
-// 	usedKeys : [],
-// 	display : [],
-// 	word : Object,
-
-// 	// HTML updates
-// 	winCounter : function() {
-// 		document.querySelector("#win-counter").innerHTML = 
-// 			"<p>Games Won: </p> " +
-// 			"<p class='counter'>" + this.wins + "</p>";
-// 	},
-// 	guessCounter : function() {
-// 		document.querySelector("#guess-counter").innerHTML = 
-// 			"<p>Guesses Remaining: </p>" + 
-// 			"<p class='counter'>" + this.guesses + "</p>";
-// 	},
-// 	usedKeysCounter : function() {
-// 		document.querySelector("#letters-guessed").innerHTML = 
-// 			"<p>Letters Guessed: <p> " +
-// 			"<p class='counter'>" + this.usedKeys.join(", ") + "</p>";
-// 	},
-// 	wordDisplay : function() {
-// 		document.querySelector("#word-display").innerHTML = 
-// 			"<p>" + this.display.join(" ") + "</p>";
-// 	},
-// 	victory : function() {
-// 		document.querySelector("#consequence").innerHTML = 
-// 			"<h3>Victory!</h3>" +
-// 			"<p class='win-word'>" + this.wordsList.name + "</p>";
-// 	},
-// 	defeat : function() {
-// 		document.querySelector("#consequence").innerHTML = 
-// 			"<h3>Defeated!</h3>" +
-// 			"<p class='lose-word'>The word was " + this.word.name + "...</p>";
-// 	},
-
-// 	// Play sound of word
-// 	playWin : function() {
-// 		var audio = new Audio(this.word.sound);
-// 		audio.play();
-// 	},
-// 	playLose : function () {
-// 		var audio = new Audio("assets/sounds/wrong.mp3");
-// 		audio.play();
-// 	},
-
-// 	// start game
-// 	start : function() {
-// 		// set default values
-// 		this.guesses = 12;
-// 		this.display = [];
-// 		this.usedKeys = [];
-
-// 		// set up display ( _ _ _ _ _ _ )
-// 		this.word = dictionary[ Math.floor(Math.random() * dictionary.length) ];
-// 		for (var i = 0; i < this.word.name.length; i++){
-// 			this.display.push("_");
-// 		}
-
-// 		// refresh the HTML
-// 		this.winCounter();
-// 		this.guessCounter();
-// 		this.wordDisplay();
-// 		this.usedKeysCounter();
-// 	},
-
-// 	// input
-// 	input : function(lett) {
-// 		// verify a valid, unused key was pressed
-// 		if( letInput(lett) && this.usedKeys.indexOf(lett) === -1) {
-
-// 			// checker switch: turns true if a match is found
-// 			checker = false;
-
-// 			// if valid, check against the Word.name
-// 			for (var i = 0; i < this.word.name.length; i++) {
-
-// 				// if there's a match, replace a '_' in game.display
-// 				if (lett === this.word.name[i]) {
-// 					this.display[i] = lett;
-// 					checker = true;
-// 				}
-// 			}
-
-// 			// if checker's still false, take a guess point away
-// 			if (!checker) {
-// 				this.guesses--;
-// 			}
-
-// 			// add key to used keys array, sort it alphabetically
-// 			this.usedKeys.push(lett);
-// 			this.usedKeys.sort();
-
-// 			// update the HTML
-// 					this.winCounter();
-// 					this.guessCounter();
-// 					this.wordDisplay();
-// 					this.usedKeysCounter();
-
-// 			/* Win Condition (!!!):
-// 			 * no more '_'s in game.display
-// 			 */
-// 			if (this.display.indexOf('_') === -1){
-// 				this.wins++;
-// 				this.victory();
-// 				this.playWin();
-// 				this.start();
-// 			}
-
-// 			/* Lose Condition (!!!):
-// 			 * no more guesses left
-// 			 */
-// 			if (this.guesses <= 0) {
-// 				this.defeat();
-// 				this.playLose();
-// 				this.start();
-// 			}
-// 		}
-// 	}
-// }
-
-// // onload event
-// document.onLoad = game.start();
-
-// // onkeyup events
-// document.onkeyup = function(event){
-// 	var letter = String.fromCharCode(event.keyCode).toLowerCase();
-// 	game.input(letter);
-// }
+});
